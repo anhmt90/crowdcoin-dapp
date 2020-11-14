ab310d5c6a0179a0e9d71dc0abc18ab5e5621150
T.A. Ma <anhmt90@gmail.com>
2020-11-14 17:30:29 +0100

    add and style up landing page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import React, { useEffect } from 'react';
+import { Card, Button } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css'
+
 import factory from '../ethereum/factory';
+import Layout from '../components/Layout'
 
 
+
+/**
+ * useEffect won't be executed on server-side rendering (JS on browser disabled)
+ * --> no fetching data with Next.js
+ * ==> make use of getServerSideProps()
+*/
 export async function getServerSideProps() {
     /**
      * Fetch data from external API
@@ -16,20 +26,29 @@ export async function getServerSideProps() {
 
 const CampaignIndex = ({ campaigns }) => {
 
-    /**
-     * useEffect won't be executed on server-side rendering (JS on browser disabled)
-     * --> no fetching data with Next.js
-     * ==> make use of getInitialProps()
-     */
-    // useEffect(async () => {
-    //     const campaigns = await factory.methods.getDeployedCampaigns().call();
-    //     console.log(campaigns);
-    // }, []);
+    const renderCampaigns = () => {
+        const items = campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true
+            };
+        });
+
+        return <Card.Group items={items} />;
+    };
 
     return (
-        <div>
-            {campaigns[0]}
-        </div>
+        <Layout>
+            <h3>Open Campaigns</h3>
+            <Button 
+                floated="right"
+                content="Create Campaign"
+                icon="add circle"
+                primary
+            />
+            {renderCampaigns()}
+        </Layout>
     );
 
 };
